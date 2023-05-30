a61e695a11ed41d791a345bb034d57f71e87b050
Ahzam Akhtar <ahzam8186@gmail.com>
2023-05-30 23:59:16 +0530

    ..

--- cluster1/rs/wba_vault/tests/wba_vault.ts
+++ cluster1/rs/wba_vault/tests/wba_vault.ts
@@ -1,28 +1,30 @@
-import * as anchor from '@project-serum/anchor';
-import { BN, Program } from '@project-serum/anchor';
-import { WbaVault } from '../target/types/wba_vault';
-import { expect } from 'chai';
+import * as anchor from "@project-serum/anchor";
+import { BN, Program } from "@project-serum/anchor";
+import { WbaVault } from "../target/types/wba_vault";
+import { expect } from "chai";
 import {
   Connection,
   PublicKey,
   SystemProgram,
   LAMPORTS_PER_SOL,
-} from '@solana/web3.js';
+} from "@solana/web3.js";
 import {
   TOKEN_PROGRAM_ID,
   createMint,
   getOrCreateAssociatedTokenAccount,
   mintTo,
-} from '@solana/spl-token';
-import { parseMintAccount } from '@metaplex-foundation/js';
+} from "@solana/spl-token";
+import { parseMintAccount } from "@metaplex-foundation/js";
 
-describe('wbavault', async () => {
-  anchor.AnchorProvider.env().opts.commitment = 'confirmed';
+describe("wbavault", async () => {
+  anchor.AnchorProvider.env().opts.commitment = "confirmed";
   anchor.setProvider(anchor.AnchorProvider.env());
   const provider = anchor.getProvider();
-  const programId = new anchor.web3.PublicKey("53SX2j4x8UaKM7fdy8C1UWUwhmMbKVBpsHRQVKLvHcwP");
+  const programId = new anchor.web3.PublicKey(
+    "53SX2j4x8UaKM7fdy8C1UWUwhmMbKVBpsHRQVKLvHcwP"
+  );
   const program = anchor.workspace.WbaVault as Program<WbaVault>;
-  console.log(program.programId)
+  console.log(program.programId);
   // Generate new keypair
   const keypair = anchor.web3.Keypair.generate();
 
@@ -30,16 +32,22 @@ describe('wbavault', async () => {
   const vaultState = anchor.web3.Keypair.generate();
 
   // Create the PDA for our vault auth
-  const vaultAuthSeeds = [Buffer.from('auth'), vaultState.publicKey.toBuffer()];
+  const vaultAuthSeeds = [Buffer.from("auth"), vaultState.publicKey.toBuffer()];
 
-  const vaultAuthKey = anchor.web3.PublicKey.findProgramAddressSync(vaultAuthSeeds, program.programId)[0];
+  const vaultAuthKey = anchor.web3.PublicKey.findProgramAddressSync(
+    vaultAuthSeeds,
+    program.programId
+  )[0];
   // Create the PDA for our vault
-  const vaultSeeds = [Buffer.from('vault'), vaultAuthKey.toBuffer()];
-  const vaultKey = anchor.web3.PublicKey.findProgramAddressSync(vaultSeeds, program.programId)[0];
+  const vaultSeeds = [Buffer.from("vault"), vaultAuthKey.toBuffer()];
+  const vaultKey = anchor.web3.PublicKey.findProgramAddressSync(
+    vaultSeeds,
+    program.programId
+  )[0];
 
   let mint = null;
 
-  it('Airdrop token', async () => {
+  it("Airdrop token", async () => {
     const txhash = await provider.connection.requestAirdrop(
       keypair.publicKey,
       2 * LAMPORTS_PER_SOL
@@ -53,7 +61,7 @@ describe('wbavault', async () => {
     });
   });
 
-  it('Create Mint', async () => {
+  it("Create Mint", async () => {
     mint = await createMint(
       provider.connection,
       keypair,
@@ -90,10 +98,10 @@ describe('wbavault', async () => {
       tokenAccount.value[0].pubkey
     );
 
-    expect('1000000000').to.equal(tokenAmount.value.amount);
+    expect("1000000000").to.equal(tokenAmount.value.amount);
   });
 
-  it('Is initialized', async () => {
+  it("Is initialized", async () => {
     const txhash = await program.methods
       .initialize()
       .accounts({
@@ -105,9 +113,10 @@ describe('wbavault', async () => {
       })
       .signers([keypair, vaultState])
       .rpc();
+    console.log(txhash);
   });
 
-  it('Deposit', async () => {
+  it("Deposit", async () => {
     const vaultBeforeBalance = await provider.connection.getBalance(vaultKey);
     const txhash = await program.methods
       .deposit(new BN(0.1 * LAMPORTS_PER_SOL))
@@ -120,7 +129,7 @@ describe('wbavault', async () => {
       })
       .signers([keypair])
       .rpc();
-
+    console.log(txhash);
     const vaultAfterBalance = await provider.connection.getBalance(vaultKey);
 
     expect(vaultAfterBalance).to.equal(
@@ -128,7 +137,7 @@ describe('wbavault', async () => {
     );
   });
 
-  it('Withdraw', async () => {
+  it("Withdraw", async () => {
     const vaultBeforeBalance = await provider.connection.getBalance(vaultKey);
     const txhash = await program.methods
       .withdraw(new BN(0.1 * LAMPORTS_PER_SOL))
@@ -140,12 +149,10 @@ describe('wbavault', async () => {
         systemProgram: SystemProgram.programId,
       })
       .signers([keypair])
-      .rpc(
-        {
-          skipPreflight : true
-        }
-      );
-
+      .rpc({
+        skipPreflight: true,
+      });
+    console.log(txhash);
     const vaultAfterBalance = await provider.connection.getBalance(vaultKey);
 
     expect(vaultAfterBalance).to.equal(
@@ -153,7 +160,7 @@ describe('wbavault', async () => {
     );
   });
 
-  it('Deposit SPL', async () => {
+  it("Deposit SPL", async () => {
     let ownerAta = await getOrCreateAssociatedTokenAccount(
       provider.connection,
       keypair,
@@ -174,7 +181,7 @@ describe('wbavault', async () => {
       .accounts({
         owner: keypair.publicKey,
         vaultState: vaultState.publicKey,
-        vaultAuth : vaultAuthKey,
+        vaultAuth: vaultAuthKey,
         ownerAta: ownerAta.address,
         vaultAta: vaultAta.address,
         tokenMint: mint,
@@ -183,7 +190,7 @@ describe('wbavault', async () => {
       })
       .signers([keypair])
       .rpc();
-
+    console.log(txhash);
     const tokenAccount =
       await provider.connection.getParsedTokenAccountsByOwner(vaultAuthKey, {
         mint: mint,
@@ -193,10 +200,10 @@ describe('wbavault', async () => {
       tokenAccount.value[0].pubkey
     );
 
-    expect('100000000').to.equal(tokenAmount.value.amount);
+    expect("100000000").to.equal(tokenAmount.value.amount);
   });
 
-  it('Withdraw SPL', async () => {
+  it("Withdraw SPL", async () => {
     let ownerAta = await getOrCreateAssociatedTokenAccount(
       provider.connection,
       keypair,
@@ -222,11 +229,11 @@ describe('wbavault', async () => {
         vaultAta: vaultAta.address,
         tokenMint: mint,
         tokenProgram: TOKEN_PROGRAM_ID,
-        systemProgram : SystemProgram.programId,
+        systemProgram: SystemProgram.programId,
       })
       .signers([keypair])
       .rpc();
-
+    console.log(txhash);
     // const tokenAccount =
     //   await provider.connection.getParsedTokenAccountsByOwner(
     //     keypair.publicKey,
